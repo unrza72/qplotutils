8c9862974b745f5ac5411fb6309345c69a65482f
Philipp Baust <philipp.baust@gmail.com>
2018-06-29 05:57:46 +0200

    Python3 compatibility

--- qplotutils/ui/resources_rc.py
+++ qplotutils/ui/resources_rc.py
@@ -2,14 +2,13 @@
 
 # Resource object code
 #
-# Created: Do. Okt 8 09:37:35 2015
-#      by: The Resource Compiler for PyQt (Qt v4.8.6)
+# Created by: The Resource Compiler for PyQt4 (Qt v4.8.7)
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore
 
-qt_resource_data = "\
+qt_resource_data = b"\
 \x00\x00\x0b\x9a\
 \x3c\
 \x3f\x78\x6d\x6c\x20\x76\x65\x72\x73\x69\x6f\x6e\x3d\x22\x31\x2e\
@@ -388,7 +387,7 @@ qt_resource_data = "\
 \x73\x76\x67\x3e\x0a\
 "
 
-qt_resource_name = "\
+qt_resource_name = b"\
 \x00\x05\
 \x00\x68\xc4\x98\
 \x00\x62\
@@ -403,7 +402,7 @@ qt_resource_name = "\
 \x00\x6c\x00\x6f\x00\x73\x00\x65\
 "
 
-qt_resource_struct = "\
+qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02\
 \x00\x00\x00\x24\x00\x00\x00\x00\x00\x01\x00\x00\x0b\x9e\
